57ef71eac45d2b9a1d083647624eca311eadf1bb
Mislav Sagovac <33196375+MislavSag@users.noreply.github.com>
2020-04-14 13:53:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import find_packages, setup
 setup(
     name='trademl',
     version='0.1',
-    packages=['trdeml', 'trademl.modeling', 'trademl.strategies']
+    packages=['trademl', 'trademl.modeling', 'trademl.strategies']
 )
