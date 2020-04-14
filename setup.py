8e35e37026cc690717ed3beec05f3ee840f9f4d0
Mislav Sagovac <33196375+MislavSag@users.noreply.github.com>
2020-04-14 13:53:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import find_packages, setup
 setup(
     name='trademl',
     version='0.1',
-    packages=find_packages()  # will return a list ['spam', 'spam.fizz']
+    packages=['trdeml', 'trademl.modeling', 'trademl.strategies']
 )
