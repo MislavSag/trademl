033af07c75ddaeffe0c67d7ea122f0ae8e1b6ddf
MislavSag <mislav.sagovac@contentio.biz>
2020-04-23 21:54:22 +0200

    fix remove_ohlc_ooutliers function.

--- trademl/modeling/outliers.py
+++ trademl/modeling/outliers.py
@@ -11,12 +11,12 @@ def remove_ohlc_ouliers(data, threshold_up=1, threshold_down=-0.3):
     """
     ohlcCols = ["open", 'high', 'low', 'close']
     data = data.loc[
-        (data[ohlcCols].pct_change(1) < threshold_up).all(1) &
-        (data[ohlcCols].pct_change(1).shift(-1) > threshold_down).all(1)
+        (data[ohlcCols].pct_change(1, freq='Min') < threshold_up).all(1) &
+        (data[ohlcCols].pct_change(1, freq='Min').shift(-1) > threshold_down).all(1)
     ]
     data = data.loc[
-        (data[ohlcCols].pct_change(1) > threshold_down).all(1) &
-        (data[ohlcCols].pct_change(1).shift(-1) < threshold_up).all(1)
+        (data[ohlcCols].pct_change(1, freq='Min') > threshold_down).all(1) &
+        (data[ohlcCols].pct_change(1, freq='Min').shift(-1) < threshold_up).all(1)
     ]
 
     return data
