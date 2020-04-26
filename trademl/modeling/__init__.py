af1ae1086bd52f6dd1d3c32663e9f49a2d9fa07b
MislavSag <mislav.sagovac@contentio.biz>
2020-04-26 19:17:03 +0200

    change init

--- trademl/modeling/__init__.py
+++ trademl/modeling/__init__.py
@@ -14,3 +14,5 @@ from trademl.modeling.utils import (
     cbind_pandas_h2o)
 from trademl.modeling.features import (
     add_ind, add_ind_df, add_technical_indicators, add_fourier_transform)
+from trademl.modeling.backtest import (
+    cumulative_returns, cumulative_returns_tb, minute_to_daily_return)
