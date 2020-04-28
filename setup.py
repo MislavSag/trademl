7d1f4b248e7a37b25d32db586ffc28cb52ac2b79
MislavSag <mislav.sagovac@contentio.biz>
2020-04-28 08:46:22 +0200

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='trademl',
-    version='0.1.1',
+    version='0.1.2',
     license='MIT',
     author='Mislav Sagovac',
     author_email='mislav.sagovac@contenio.biz',
